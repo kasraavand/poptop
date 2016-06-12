83851cf6e610b6d357a50339b021927e3251a162
kasramvd <kasraavand@gmail.com>
2016-06-12 19:35:02 +0430

    update with new version of scrapy

--- __init__.py
+++ __init__.py
@@ -1,30 +1,24 @@
 from pdfcreator import runconvert
-from twisted.internet import reactor
-from scrapy.crawler import Crawler
-from scrapy import log, signals
-#from testspiders.spiders.followall import FollowAllSpider
-from scrapy.utils.project import get_project_settings
+from scrapy.crawler import CrawlerProcess
 from poptop.spiders.stackoverflow_spider import MySpider
 import sys
 import os
 
-settings = get_project_settings()
-crawler = Crawler(settings)	
-crawler.signals.connect(reactor.stop, signal=signals.spider_closed)
-crawler.configure()
-d=raw_input('Enter the date with this format (d-m-Y) : ')
-q_number=raw_input('What number of question you want to get? : ')
-spider=MySpider(date=d)
-crawler.crawl(spider)
-crawler.start()
-log.start()
-reactor.run() # the script will block here until the spider_closed signal was sent
+process = CrawlerProcess({
+    'USER_AGENT': 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)'
+})
+
+# crawler.configure()
+d = raw_input('Enter the date with this format (d-m-Y) : ')
+q_number = raw_input('What number of question you want to get? : ')
+process.crawl(MySpider, date=d)
+process.start()  # the script will block here until the spider_closed signal was sent
 
 try:
-    os.makedirs(sys.path[0]+'/questions')
+    os.makedirs(sys.path[0] + '/questions')
 except OSError:
     pass
 
-for url in open('url{}.txt'.format(d),'r') :
-	runconvert('http://stackoverflow.com'+url.strip())
+for url in open('url{}.txt'.format(d), 'r'):
+    runconvert('http://stackoverflow.com' + url.strip())
 sys.exit()
